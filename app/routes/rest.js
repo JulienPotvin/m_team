3ddcdc7562c07df79945584da8576be45c9f9094
Julien Potvin <jpotvin@mnubo.com>
2015-11-28 11:13:04 -0500

    Dummy endpoint for demand

--- app/routes/rest.js
+++ app/routes/rest.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var stations = require('../json/stations.json');
+var demand = require('../json/demand.json');
+
 
 /* GET users listing. */
 router.get('/stations', function(req, res, next) {
   res.json(stations);
+});
+
+router.get('/demand', function(req, res, next) {
+  res.json(demand);
 });
 
 module.exports = router;
